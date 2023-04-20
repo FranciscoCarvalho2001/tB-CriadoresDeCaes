17e48533657e238d01323679fe039d541e2bbbe9
FranciscoCarvalho2001 <101589943+FranciscoCarvalho2001@users.noreply.github.com>
2023-04-20 16:14:36 +0100

    Adição de anotadores no Model

--- CriadorCaes/Models/Criadores.cs
+++ CriadorCaes/Models/Criadores.cs
@@ -1,4 +1,7 @@
-﻿namespace CriadorCaes.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace CriadorCaes.Models
 {
     /// <summary>
     /// descrição do criador de cães
@@ -17,31 +20,39 @@
         /// <summary>
         /// nome do criador
         /// </summary>
+        [Required(ErrorMessage ="O {0} é de preenchimento obrigatório")]
+        [Display(Name = "Nome")]
         public string Nome { get; set;}
 
         /// <summary>
         /// nome comercial do criador de cães
         /// </summary>
+        [Display(Name = "Nome Criador")]
         public string NomeComercial { get; set;}
 
         /// <summary>
         /// morada
-        /// </summary>
-        public string Morada { get; set;}  
+        /// </summary>     
+        public string Morada { get; set;}
 
         /// <summary>
         /// Código postal
         /// </summary>
+        [Display(Name = "Código Postal")]
         public string CodPostal { get; set;}
 
         /// <summary>
         /// email do criador
         /// </summary>
+        [EmailAddress(ErrorMessage ="O {0} não está escrito corretamente")]
+        [Required(ErrorMessage = "O {0} é de preenchimento obrigatório")]
         public string Email { get; set;}
 
         /// <summary>
         /// telemóvel do criador
         /// </summary>
+        [Required(ErrorMessage = "O {0} é de preenchimento obrigatório")]
+        [Display(Name ="Telemóvel")]
         public string Telemovel { get; set; }
        
         /// <summary>
